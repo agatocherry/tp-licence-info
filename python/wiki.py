580b36ad099d672c82b62b4015b60207323fa9a0
agatocherry <agatocherry@gmail.com>
2024-12-13 15:47:49 +0100

    Add `wiki.py`

---
+++ python/wiki.py
@@ -0,0 +1,9 @@
+import wikipedia
+
+wikipedia.set_lang("fr")
+
+print( wikipedia.summary("Philosophie"))
+print()
+print( wikipedia.summary("Python (programming language)"))
+print( wikipedia.summary("Galaxie"))
+print()
